d7bac01db1010628a977445ba3a25c7ee3b0d80a
irumaru <jibakomaorochi13@outlook.jp>
2024-09-14 07:50:52 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+var redisUrl = Environment.GetEnvironmentVariable("REDIS_URL");
+
+Console.WriteLine("Connection server: " + redisUrl);
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisUrl);
+var redisContext = redis.GetDatabase();
+
+var nokotan = redisContext.StringGet("nokotan");
+Console.WriteLine(nokotan);
+if(nokotan.IsNullOrEmpty)
+{
+  Console.WriteLine("nokotan is not found.");
+}
+
+redisContext.StringSet("shika", "しかのこのこのここしたんたん");
+
+var shika = redisContext.StringGet("shika");
+Console.WriteLine(shika);
